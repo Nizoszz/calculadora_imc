376cd58a1e15259a4a086d493c495f49dd668ca5
Andrew <andrewairamdasilva@gmail.com>
2024-04-08 20:50:42 -0300

    fix: abstract method imprimir resultado

--- lib/calculadora_imc.dart
+++ lib/calculadora_imc.dart
@@ -1,3 +1,5 @@
+import 'package:calculadora_imc/validar_resultado.dart';
+
 import 'calcular_imc.dart';
 import 'package:calculadora_imc/console_utils.dart';
 
@@ -6,11 +8,12 @@ void execute(){
   print('Calculadora de IMC');
   var terminal = ConsoleUtils();
   var pessoa = terminal.lerDadosDoTerminal();
+  var validar = ValidarResultado();
 
   if (pessoa != null){
       var imcCalculator = CalcularIMC();
       var imc = imcCalculator.calcularIMC(pessoa);
-      terminal.imprimirResultado(imc, pessoa.nome);
+      validar.imprimir(imc, pessoa.nome);
   } else {
     print('Não foi possível calcular o IMC.');
   }
